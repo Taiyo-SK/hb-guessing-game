b5b680920ae339e418997aedbc39513ef2eba7ff
Taiyo Scanlon-Kimura <taiyo.scanlon-kimura@fulbrightmail.org>
2023-04-04 09:27:36 -0700

    Co-authored-by: ndrygas <ndrygas@users.noreply.github.com> Co-authored-by: Ali Eljaouhari <ElAbdelali@users.noreply.github.com>

--- game.py
+++ game.py
@@ -11,16 +11,28 @@ def playGame():
     guess = 0
 
     while guess != randomInt:
-        guess = int(input("Your guess? "))
-        if guess > randomInt:
-            print("Your guess is too high, try again.")
-            tries += 1
-           
-        elif guess < randomInt:
-            print("Your guess is too low, try again.")
-            tries += 1
-        else:
-            tries += 1
-            print("Congratulations ", name, "! You found my number in ", tries, "tries!")
+        try: 
+            guess = int(input("Your guess? "))
+        
+            if guess < 1 or guess > 100:
+                print("Your guess should be between 1 and 100. Don't be silly.")
+        
+            elif guess > randomInt:
+                print("Your guess is too high, try again.")
+                tries += 1
+        
+            elif guess < randomInt:
+                print("Your guess is too low, try again.")
+                tries += 1
+        
+            elif guess < 1 or guess > 100:
+                print("Your guess should be between 1 and 100. Don't be silly.")
+        
+            else:
+                tries += 1
+                print("Congratulations ", name, "! You found my number in ", tries, "tries!")
+        
+        except ValueError:
+            print("Invalid input, please enter an Integer.")
 
 playGame()
